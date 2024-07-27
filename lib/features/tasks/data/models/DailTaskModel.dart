1155536fbc04040a3fa6953226e2c95ac3002402
mouloud240 <m.hasrane@esi-sba.dz>
2024-07-27 02:07:38 +0100

    added unit tests to dailyTaskmodel

--- lib/features/tasks/data/models/DailTaskModel.dart
+++ lib/features/tasks/data/models/DailTaskModel.dart
@@ -1,11 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'package:task_manager/features/tasks/domain/entities/dailyTask.dart';
 
 part 'DailTaskModel.g.dart';
 
 @HiveType(typeId: 1)
-class Dailtaskmodel extends Dailytask with HiveObjectMixin,EquatableMixin {
+DateFormat dateFormat = DateFormat("MMMM d, y 'at' h:mm:ss a");
+
+class Dailtaskmodel extends Dailytask with HiveObjectMixin, EquatableMixin {
   @HiveField(0)
   String title;
   @HiveField(1)
@@ -35,16 +38,17 @@ class Dailtaskmodel extends Dailytask with HiveObjectMixin,EquatableMixin {
   }
   factory Dailtaskmodel.fromJson(Map<String, dynamic> json) {
     return Dailtaskmodel(
-        title: json['title'],
-        description: json['description'],
-        startDate: json['startDate'].toDate(),
-        endDate: json['endDate'].toDate());
+      title: json['title'],
+      description: json['description'],
+      startDate: dateFormat.parse(json['startDate'].replaceAll(" UTC+1", "")),
+      endDate: dateFormat.parse(json["endDate"].replaceAll(" UTC+1", "")),
+    );
   }
   Map<String, dynamic> toJson() {
     return {
       "title": title,
       "description": description,
-      "startDate": startDate,
+      "startDate":startDate,
       "endDate": endDate
     };
   }
@@ -56,7 +60,7 @@ class Dailtaskmodel extends Dailytask with HiveObjectMixin,EquatableMixin {
         startDate: startDate,
         endDate: endDate);
   }
-  
+
   @override
   // TODO: implement props
   List<Object?> get props => [title, description, startDate, endDate];
---
+++ test/data/models/dailyTaskModel_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+import 'package:task_manager/features/tasks/data/models/DailTaskModel.dart';
+import 'package:task_manager/features/tasks/domain/entities/dailyTask.dart';
+import '../../helpers/json_reader.dart';
+
+DateFormat dateFormat = DateFormat("MMMM d, y 'at' h:mm:ss a");
+void main() {
+  final Map<String, dynamic> jsonMap = {
+    "title": "run",
+    "description": "Go running",
+    "startDate": "July 27, 2024 at 11:12:43 PM UTC+1",
+    "endDate": "July 26, 2024 at 11:12:28 PM UTC+1"
+  };
+
+  final testDailyTask = Dailtaskmodel(
+      title: "run",
+      description: "Go running",
+      startDate: dateFormat.parse("July 27, 2024 at 11:12:43 PM"),
+      endDate: dateFormat.parse("July 26, 2024 at 11:12:28 PM"));
+  test("Should be a subclass of DailyTask", () async {
+    expect(testDailyTask, isA<Dailytask>());
+  });
+  test("Should return a DailyTaskModel from Json", () async {
+    //araange
+    //act
+    final result = Dailtaskmodel.fromJson(jsonMap);
+    //assert
+    expect(result, testDailyTask);
+  });
+  test("should return a json from DailyTaskModel", () async {
+    //arrange
+
+    //act
+    final result = testDailyTask.toJson();
+
+    //assert
+    expect(result, jsonMap);
+  });
+}
